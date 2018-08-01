e4109e05ff5618ce17673715eb747043eaa8b2e6
Jae Yong Kim <kim900180@gmail.com>
2018-08-01 00:58:37 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,8 +61,6 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each do |season, holiday_name|
-    # binding.pry
     holiday_hash.each do | season, holiday|
       puts "#{season.capitalize!}:"
       holiday.each do |holiday, item|
